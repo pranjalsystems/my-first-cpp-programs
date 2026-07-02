95c5e0a2a8f9b26988610a6d17eb7c4d9e796262
Pranjal Kumar <pranjalkumarworkpurpose@gmail.com>
2026-07-02 22:36:57 +0530

    Day 3: Variables and expressions

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
 int main() {
-    std::cout<<"a new beginning\n";
+    //std::cout<<"a new beginning\n";
+    //std::cout<<"a "<< "new"<<" beginning";
+    //std::cout<<"it "<<"started "<<"with Python";
+    std::cout<<"a "<< "new"<<" beginning"<<std::endl;
+    std::cout<<"it "<<"started "<<"with Python" ;
     return 0;
 }
---
+++ variable.cpp
@@ -0,0 +1,20 @@
+//#include <iostream>                   //realised there cant be two int main()
+//int main() {
+//    int age=18;
+//    std::cout<<"My age"<<" is "<< age<<std::endl;
+//    std::cout<<"Next year "<<"I will be "<< age+1;
+//    return 0;
+//}
+
+//goal: 2 + 3 = 5
+//     5 × 4 = 20
+#include <iostream>
+
+int main() {
+    int a = 2;
+    int b = 3;
+    int d = 4;
+    std::cout<<a<<" + "<<b<<" = "<<a+b<<"\n";
+    std::cout<<a+b<<" x " <<d<<" = "<<(a+b)*d;
+    return 0;
+}
