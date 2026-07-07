8786a366f635919a73c1a9de4f3f525675a638e2
Pranjal Kumar <pranjalkumarworkpurpose@gmail.com>
2026-07-07 22:58:21 +0530

    Day 8: Learned the basics of for loops

---
+++ loops_for-loop.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+//while loop
+// Initialize
+
+// ↓
+
+// Condition
+
+// ↓
+
+// Body
+
+// ↓
+
+// Update
+
+// ↓
+
+// Condition
+
+// ↓
+
+// Body
+
+// ↓
+
+// Update
+
+//for loops
+// for(
+// initialize;
+// condition;
+// update
+// )
+
+int count = 1;
+int main(){
+    int count=1;
+    // while(count <= 5) 
+    // {
+    //     std::cout<<count<<"\n";
+    //     count++;
+    // }
+    // std::cout<<"\n" Can be written as for loops
+
+    for(int count = 1; count <= 5; count++)
+    {
+        std::cout << count << "\n";
+    }
+    return 0;
+}
