e7a158a5bc714aad347ad6b4dd10cfdb7f81e879
Pranjal Kumar <pranjalkumarworkpurpose@gmail.com>
2026-07-07 23:38:56 +0530

    Day 8 Project: Multiplication table using for loop

--- loops_for-loop_multiplication-table.cpp
+++ loops_for-loop_multiplication-table.cpp
@@ -1,3 +1,8 @@
+
+// ⭐ Task 1 — Rewrite Yesterday
+
+// Rewrite your multiplication table using a for loop instead of while.
+
 #include <iostream>
 int main(){
     int main_no;
@@ -6,7 +11,20 @@ int main(){
     std::cin>>main_no;
     std::cout<<"Enter upto How much you want Table Of : ";
     std::cin>>upto_no;
-    for(int count=1,count<=upto_no,count++){
-        
+    // for(int count = 1;count <= upto_no;count++){
+    //     std::cout<<main_no<<" X "<<count<<" = "<<main_no*count<<"\n";
+    // }
+
+    //IDEA----work but should i say it's a bit chunky and less clean maybe, but was trying to make jump variable from python range(start,end+1,jump)
+    // int value=main_no;
+    // for(int count=1;count<=upto_no;count++){
+    //     std::cout<<main_no<<" X "<<count<<" = "<<value<<"\n";
+    //     value+=main_no;
+    // }
+
+    //Conclusion i like previous one better (also the calculation is much faster {when the numbers become large} in c++ than python i think that's expected as its c++)
+    for(int count = 1;count <= upto_no;count++){
+        std::cout<<main_no<<" X "<<count<<" = "<<main_no*count<<"\n";
     }
+    return 0;
 }
