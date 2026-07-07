aff2b2fd64f963a006a3ff4ea8ecd20730bf4df7
Pranjal Kumar <pranjalkumarworkpurpose@gmail.com>
2026-07-07 23:40:13 +0530

    Day 8 Project: Sum of first N numbers

---
+++ loops_for-loop_sum-of-first-n-numbers.cpp
@@ -0,0 +1,18 @@
+// ⭐ Task 2 — Sum of First N Numbers
+
+
+// Example:>
+
+// Enter N: 5
+// Sum = 15
+#include <iostream>
+int main(){
+    int N;
+    int sum=0;
+    std::cout<<"Enter N: "; 
+    std::cin>>N;
+    for(int count=1;count<=N;count++){
+        sum+=count;
+    }
+    std::cout<<"Sum = "<<sum;
+}
